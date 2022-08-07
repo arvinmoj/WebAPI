6bba9fa8c7ba4a9b69045fee78de69b8e0dda4b3
Arvinmoj <arvinmojaverian@hotmail.com>
2022-08-07 11:38:26 +0430

    setup todo model resources

--- WebAPI/Core/WA.Domain/Models/ToDo/ToDoModel.cs
+++ WebAPI/Core/WA.Domain/Models/ToDo/ToDoModel.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WA.Domain.Models.ToDo;
 
 public class ToDoModel : Base.BaseEntity
@@ -6,8 +8,16 @@ public class ToDoModel : Base.BaseEntity
     {
     }
 
+    [Display(ResourceType = typeof(Resources.ToDoResources),
+            Name = nameof(Resources.ToDoResources.Title))]
     public string? Title { get; set; }
+
+    [Display(ResourceType = typeof(Resources.ToDoResources),
+        Name = nameof(Resources.ToDoResources.Description))]
     public string? Description { get; set; }
+
+    [Display(ResourceType = typeof(Resources.ToDoResources),
+        Name = nameof(Resources.ToDoResources.State))]
     public Enumerations.ToDo.States? State { get; set; }
 
 }

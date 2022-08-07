019da324793f96cb7db33fd2443ca69227b47922
Arvinmoj <arvinmojaverian@hotmail.com>
2022-08-07 10:39:04 +0430

    add error resource todo validation

--- WebAPI/Core/WA.Domain/Validations/ToDo/ToDoValidation.cs
+++ WebAPI/Core/WA.Domain/Validations/ToDo/ToDoValidation.cs
@@ -7,26 +7,26 @@ public class ToDoValidation : AbstractValidator<Models.ToDo.ToDoModel>
     {
         RuleFor(current => current.Title)
             .NotEmpty()
-            .WithMessage(errorMessage: "You did not specify {PropertyName} ")
+            .WithMessage(errorMessage: nameof(Resources.ErrorMessages.Required))
 
             .MinimumLength(4)
-            .WithMessage("{PropertyName} Must Be Longer Than 4 Characters")
+            .WithMessage(errorMessage: nameof(Resources.ErrorMessages.MinLength))
 
             .MaximumLength(20)
-            .WithMessage("{PropertyName} Must Be Less Than Or Equal To 20 Characters")
+            .WithMessage(errorMessage: nameof(Resources.ErrorMessages.MaxLength))
             ;
 
         RuleFor(current => current.Description)
             .MinimumLength(8)
-            .WithMessage("{PropertyName} Must Be Longer Than 8 Characters")
+            .WithMessage(errorMessage: nameof(Resources.ErrorMessages.MinLength))
 
             .MaximumLength(1000)
-            .WithMessage("{PropertyName} Must Be Less Than Or Equal To 1000 Characters")
+            .WithMessage(errorMessage: nameof(Resources.ErrorMessages.MaxLength))
             ;
 
         RuleFor(current => current.State)
             .IsInEnum()
-            .WithMessage("{PropertyName} has a range of values which does not include {PropertyValue}")
+            .WithMessage(errorMessage: nameof(Resources.ErrorMessages.IsInEnum))
             ;
     }
 }

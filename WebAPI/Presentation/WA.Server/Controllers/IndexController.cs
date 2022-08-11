8f01ee89281d1a36110d779076ef3c4f360556e0
Arvinmoj <arvinmojaverian@hotmail.com>
2022-08-11 10:13:17 +0430

    setup indexcontroller update and delete action

--- WebAPI/Presentation/WA.Server/Controllers/IndexController.cs
+++ WebAPI/Presentation/WA.Server/Controllers/IndexController.cs
@@ -1,18 +1,30 @@
-using Models;
-using Microsoft.AspNetCore.Mvc;
 using Data;
+using DTOs.ToDo;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 namespace WA.API.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 public class IndexController : Infrastructure.BaseApiControllerWithDatabase
 {
-
-    public IndexController(IUnitOfWork unitOfWork) : base(unitOfWork)
+    public IndexController(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork)
     {
+        Mapper = mapper;
+    }
+    protected IMapper Mapper { get; }
+
+
+    [HttpGet(template: "GetById")]
+    public async Task<ActionResult<IEnumerable<Models.ToDoModel>>> GetByIdAsync(Guid id)
+    {
+        var result =
+            await UnitOfWork.ToDoRepository.GetByIdAsync(id);
+
+        return Ok(value: result);
     }
 
-    [HttpGet("GetAll")]
+    [HttpGet]
     public
     async Task<ActionResult<IEnumerable<Models.ToDoModel>>> GetAllAsync()
     {
@@ -23,23 +35,72 @@ public class IndexController : Infrastructure.BaseApiControllerWithDatabase
     }
 
     [HttpPost]
-    public
-    async Task<ActionResult<Models.ToDoModel>> PostAsync(DTOs.ToDo.ToDoViewModel viewModel)
+    public async Task<ActionResult<IEnumerable<Models.ToDoModel>>> PostAsync(ToDoViewModel viewModel)
     {
         try
         {
-          var newEntity =
-            new Models.ToDoModel
-            {
-                Title = viewModel.Title,
-                State = viewModel.State,
-                Description = viewModel.Description,
-            };   
+           Models.ToDoModel toDoModel=
+                Mapper.Map<Models.ToDoModel>(source: viewModel);
 
-            await UnitOfWork.ToDoRepository.InsertAsync(newEntity);
+            await UnitOfWork.ToDoRepository.InsertAsync(toDoModel);
             await UnitOfWork.SaveAsync();
 
-            return Ok(value: newEntity);
+            return Ok(value: toDoModel);
+        }
+        catch (System.Exception)
+        {
+            return Ok(value: null);
+        }
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<IEnumerable<Models.ToDoModel>>> PutAsync(ToDoViewModel viewModel)
+    {
+        try
+        {
+            Models.ToDoModel toDoModel=
+                Mapper.Map<Models.ToDoModel>(source: viewModel);
+
+            await UnitOfWork.ToDoRepository.UpdateAsync(toDoModel);
+            await UnitOfWork.SaveAsync();
+
+            return Ok(value: toDoModel);
+        }
+        catch (System.Exception)
+        {
+            return Ok(value: null);
+        }
+    }
+
+    [HttpDelete(template: "DeleteByID")]
+    public async Task<ActionResult<IEnumerable<Models.ToDoModel>>> DeleteByIDAsync(Guid id)
+    {
+        try
+        {
+            var result =
+                await UnitOfWork.ToDoRepository.DeleteByIdAsync(id);
+            await UnitOfWork.SaveAsync();
+
+            return Ok(value: result);
+        }
+        catch (System.Exception)
+        {
+            return Ok(value: null);
+        }
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult<IEnumerable<Models.ToDoModel>>> DeleteAsync(ToDoViewModel viewModel)
+    {
+        try
+        {
+            Models.ToDoModel toDoModel=
+                Mapper.Map<Models.ToDoModel>(source: viewModel);
+
+            await UnitOfWork.ToDoRepository.DeleteAsync(toDoModel);
+            await UnitOfWork.SaveAsync();
+
+            return Ok(value: toDoModel);
         }
         catch (System.Exception)
         {
